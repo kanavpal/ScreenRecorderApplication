7629eba425cc8940b5c7e7b59f619ce94004f6cc
Akki019 <Akshit.105013@stu.upes.ac.in>
2023-09-25 18:49:17 +0530

    Setup Django

--- SSApp/apps.py
+++ SSApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SrappConfig(AppConfig):
+class SsappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SRApp'
+    name = 'SSApp'
